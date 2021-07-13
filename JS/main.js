1dafb7521545565b9307328afd5f8075083ac78f
pratiksha kale <pratikshakale909@gmail.com>
2021-07-14 00:04:05 +0530

    game ui

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,55 @@
-import Index  from "./index.js";
-import Indexview from './indexview.js';
+// import Index  from "./index.js";
+// import Indexview from './indexview.js';
+
+// let index = new Index()
+// let indexView = new Indexview();
+
+// document.querySelector('.start')
+// .addEventListener('click',()=>{
+//     console.log("new Gamne")
+//     onStartClick()
+// })
+// let tiles = document.querySelectorAll(".board-title")
+// // console.log(tiles)
+// tiles.forEach((tile)=>{
+//   tile.addEventListener('click',()=>{
+//     // console.log(tile.dataset.index)
+//     onTileClick(tile.dataset.index)
+//   })
+// })
+
+// function onTileClick(i){
+// // do something
+//         index.makeMove(i)
+//         // make a move
+//         // update board
+//        indexView.updateBoard(index)
+// }
+
+// function onStartClick(){
+//     index = new Index()
+//     indexView.updateBoard(index)
+// }
+
+// indexView.updateBoard(index)
+
+
+import Index from './index.js'
+import Indexview from './indexview.js'
 
 let index = new Index()
-let indexView = new Indexview();
-
-document.querySelector('.start')
-.addEventListener('click',()=>{
-    console.log("new Gamne")
-    onStartClick()
-})
-let tiles = document.querySelectorAll(".board-title")
-// console.log(tiles)
-tiles.forEach((tile)=>{
-  tile.addEventListener('click',()=>{
-    // console.log(tile.dataset.index)
-    onTileClick(tile.dataset.index)
-  })
-})
-
-function onTileClick(i){
-// do something
-        index.makeMove(i)
-        // make a move
-        // update board
-       indexView.updateBoard(index)
-}
-
-function onStartClick(){
-    index = new Index()
-    indexView.updateBoard(index)
-}
+let indexView = new Indexview()
 
 indexView.updateBoard(index)
+console.log(index.board)
+console.log('Main turn',index.turn)
+index.nextTurn();
+console.log('Main turn',index.turn)
+index.makeMove(2)
+console.log(index.board)
+index.nextTurn()
+index.makeMove(4)
+console.log(index.board)
+
+indexView.updateBoard(index)
+
