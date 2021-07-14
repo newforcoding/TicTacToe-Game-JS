028777c8d5b9450a493faaa96be62c884ef8e079
pratiksha kale <pratikshakale909@gmail.com>
2021-07-15 00:07:30 +0530

    game combinations functionality

--- JS/index.js
+++ JS/index.js
@@ -71,7 +71,46 @@ export default class Index{
       }
   }
   makeMove(i){
+      if(this.endOfGame()){
+          return;
+      }
+      if(this.board[i]){
+          return;
+      }
       this.board[i]=this.turn
+      let winningCombinations = this.findWinningCombinations()
+    if(!winningCombinations){
+        this.nextTurn()
+    }
+ }
+
+  findWinningCombinations(){
+      const winningCombinations = [
+          [0,1,2],[3,4,5],[6,7,8],[0,3,6],
+          [1,4,7],[2,5,8],[0,4,8],[2,4,6]
+      ]
+
+  for(const combination of winningCombinations){
+      const [a,b,c] = combination
+
+      if(this.board[a] && (this.board[a]===this.board[b]
+        && this.board[a]=== this.board[c])){
+            return combination;
+        }
+    }    
+     return null;
   }
+
+  endOfGame(){
+      let winningCombinations = this.findWinningCombinations()
+      if(winningCombinations){
+          return true;
+      }
+      else{
+          return false
+      }
+  }
+
+
 
 }
