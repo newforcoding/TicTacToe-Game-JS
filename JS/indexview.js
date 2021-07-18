13f48e0585f1b8e52dee0a280282752f57f5d949
pratiksha kale <pratikshakale909@gmail.com>
2021-07-18 16:05:16 +0530

    board updated with winning combinations

--- JS/indexview.js
+++ JS/indexview.js
@@ -1,53 +1,39 @@
-// export default class Indexview{
-     
-// updateBoard(index){
-//      this.updateTurn(index);
-//      const winningCombinations = index.findWinnningCombinations()
-      
-//      for(let i=0;i<index.board.length;i++){
-//         const tile = document.querySelector(`.board-title 
-//         [data-index='${i}']`); 
-
-//         tile.classList.remove('tile-winner')
-
-//        let tileType = index.board[i] == 'X' ? "tile-x" : "tile-o";
-        
-//        tile.innerHTML = `<span class="${tileType}">${index.board[i] ? index.board[i] : ""}</span>`
-      
-//        if(winningCombinations && winningCombinations.includes(i)){
-//            tile.classList.add('tile-winner')
-//            console.log(tile.classList.add('tile-winner'))
-//        }
-//     }
-
-//  }
-
-//  updateTurn(index){
-//      let playerX = document.querySelector('.player-x')
-//      let playerO = document.querySelector('.player-o')
-     
-//      playerX.classList.remove('active')
-//      playerO.classList.remove('active')
-     
-//      if(index.turn == 'X'){
-//          playerX.classList.add('active')
-//      }else{
-//          playerO.classList.add('active')
-//      }
-//  }
-
-// }
-
-
 export default class Indexview{
- constructor(){
-     console.log('GamView Board')
- }
+
  updateBoard(index){
+     this.updateTurn(index);
+    const winningCombination = index.findWinningCombinations()
+    console.log(winningCombination)
      for(let i=0;i<index.board.length;i++){
          const tile = document.querySelector(`.board-title[data-index='${i}']`)
         //  console.log(tile)
-        tile.textContent = index.board[i]
+
+        tile.classList.remove('tile-winner')
+        
+        const tileType = index.board[i] == 'X' ?
+        'tile-x' : 'tile-o';
+
+        tile.innerHTML = `<span class="${tileType}">
+        ${index.board[i] ? index.board[i]: ""}</span>`
+
+        if(winningCombination && winningCombination.includes(i)){
+            tile.classList.add('tile-winner');
+        }
+        
+    }
+ }
+ updateTurn(index){
+     let playerX=document.querySelector('.player-x');
+     let playerO=document.querySelector('.player-o');
+    
+    playerX.classList.remove("active")
+    playerO.classList.remove("active")
+
+     if(index.turn == 'X'){
+         playerX.classList.add('active')
+     }
+     else{
+         playerO.classList.add('active')
      }
  }
 }
